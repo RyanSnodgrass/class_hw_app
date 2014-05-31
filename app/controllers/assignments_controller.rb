0837f2ed146a29e254893a81792fa75927cc6fe4
RyanSnodgrass <res0428@yahoo.com>
2014-05-31 19:00:47 -0400

    added assignments show page

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -1,4 +1,8 @@
 class AssignmentsController < ApplicationController
+	def show
+		@assignment = Assignment.find(params[:id])
+		@cohort = Cohort.find(@assignment.cohort_id)
+	end
 	def create
 		@new_assignment = Assignment.new(assignment_params)
 		if @new_assignment.save
@@ -7,7 +11,17 @@ class AssignmentsController < ApplicationController
 			redirect_to :back
 		end
 	end
+
+	def edit
+		@assignment = Assignment.find(params[:id])
+	end
 	def update
+		@assignment = Assignment.find(params[:id])
+		if @assignment.update_attributes(assignment_params)
+			redirect_to assignment_path(@assignment.id)
+		else
+			redirect_to edit_assignment_path(@assignment.id)
+		end
 	end
 
 	private
