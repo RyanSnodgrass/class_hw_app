48f518d69c91683fb8b91ad09d5d2819dd0aab87
RyanSnodgrass <res0428@yahoo.com>
2014-06-03 19:01:49 -0400

    rspec on cohorts index

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -6,14 +6,22 @@ FactoryGirl.define do
 		role "admin"
 		first_name "Sarah"
 		last_name "Lodato"
-
-		trait :teacher do
-			role "teacher"
-		end
-
-		trait :student do
-			role "student"
-		end
+	end
+	factory :teacher, class: User do
+		email "shelby.scwitzer@teacher.com"
+		password "password"
+		password_confirmation "password"
+		role "teacher"
+		first_name "Shelby"
+		last_name "Schwitzer"
+	end
+	factory :student, class: User do
+		email "ryan.snodgrass@student.com"
+		password "password"
+		password_confirmation "password"
+		role "student"
+		first_name "Ryan"
+		last_name "Snodgrass"
 	end
 	# factory(:teacher_user) do
 	# 	email "shelby.scwitzer@teacher.com"
